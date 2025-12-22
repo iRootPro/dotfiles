202ba5d3b2527bc6a55c66a46611c34d58e11a81
Alexander Neupokoev <alexsatter@gmail.com>
2025-12-22 11:43:05 +0300

    update config

---
+++ .config/nvim-lazy/lua/plugins/cat.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = false,
+        show_end_of_buffer = false,
+        term_colors = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          telescope = true,
+          notify = false,
+          mini = false,
+          lsp_trouble = true,
+          mason = true,
+          which_key = true,
+          treesitter = true,
+          native_lsp = {
+            enabled = true,
+            underlines = {
+              errors = { "undercurl" },
+              hints = { "undercurl" },
+              warnings = { "undercurl" },
+              information = { "undercurl" },
+            },
+          },
+        },
+      })
+
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+}
